a183e8d0f3550f80e7082d435fb3f0ce31b49288
Xi Zhang <xixixixizhang28@gmail.com>
2021-04-21 20:58:47 +1000

    Create a AdChip for categories

--- client/src/pages/Ads.tsx
+++ client/src/pages/Ads.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import AdCard from "../components/AdCard"
 import {mockData} from "../mockData"
+import AdChip from "../components/AdChip"
 const Ads = () => {
   return (
     <div id="main">
       <h1>Ads</h1>
-      {mockData.map((data:Ad) => {
+      {/* {mockData.map((data:Ad) => {
         return <AdCard {...data}/>
-        })}
+        })} */}
+      <AdChip></AdChip>
     </div>
   );
 };
