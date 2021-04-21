4ee092a342396ccd828effcee2d5254ea646dab9
azha0018 <azha0018@student.monash.edu>
2021-04-21 21:17:18 +1000

    ad chips fix

--- client/src/components/AdChip.tsx
+++ client/src/components/AdChip.tsx
@@ -1,55 +1,52 @@
-import React from 'react';
-import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
-import Avatar from '@material-ui/core/Avatar';
-import Chip from '@material-ui/core/Chip';
-import {mockCategoryData} from "../mockData"
+import React, { useState } from "react";
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+import Chip from "@material-ui/core/Chip";
+import { mockCategoryData } from "../mockData";
+import { couldStartTrivia } from "typescript";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      display: 'flex',
-      justifyContent: 'center',
-      flexWrap: 'wrap',
-      '& > *': {
+      display: "flex",
+      justifyContent: "center",
+      flexWrap: "wrap",
+      "& > *": {
         margin: theme.spacing(0.5),
       },
     },
-  }),
+  })
 );
 
-
-
-export default function Chips() {
+const Chips = () => {
   const classes = useStyles();
 
-  const [categoryData, setCategoryData] = React.useState<CategoryData[]>(
+  const [categoryData, setCategoryData] = useState<CategoryData[]>(
     mockCategoryData
-)
-
-  const handleDelete = () => {
-    console.info('You clicked the delete icon.');
-  };
+  );
 
   const handleClick = (categoryIndex: number) => {
-    setCategoryData(
-        categoryData => {
-            categoryData[categoryIndex].select = !categoryData[categoryIndex].select
-            console.log(categoryData)
-            return categoryData
-        }
-    )
+    setCategoryData((oldData) => {
+      let data = oldData.slice();
+      data[categoryIndex].select = !data[categoryIndex].select;
+      return data;
+    });
   };
 
   return (
     <div className={classes.root}>
-        {categoryData.map((category, i) => {
-            if(category.select === false){
-                return <Chip variant="outlined" label={category.name} onClick={() => {handleClick(i)}} key={i}/>
-            }
-            else{
-                return <Chip label={category.name} onClick={() => {handleClick(i)}} key={i}/>
-            }
-        })}
+      {categoryData.map((category, i) => (
+        <Chip
+          variant={category.select ? "default" : "outlined"}
+          label={category.name}
+          onClick={() => {
+            handleClick(i);
+          }}
+          key={i}
+        />
+      ))}
     </div>
   );
-}
+};
+
+export default Chips;
