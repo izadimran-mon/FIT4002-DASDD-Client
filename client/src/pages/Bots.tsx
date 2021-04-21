d83514d2103f47a7c4bbcfaa575510dc39f2e742
Anu Wickramasinghe <52063196+Anuwicks@users.noreply.github.com>
2021-04-21 23:09:02 +1000

    added react table base

--- client/src/pages/Bots.tsx
+++ client/src/pages/Bots.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import BotsTable from "../components/BotsTable";
 const Bots = () => {
   return (
-    <div id="main">
+    <div id='main'>
       <h1>Bots</h1>
+      <BotsTable />
     </div>
   );
 };
