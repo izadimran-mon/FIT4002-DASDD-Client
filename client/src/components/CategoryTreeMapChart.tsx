fc1e62a27b827782167622a0cef8c48c97a58ec4
Uyen Tran <utra0001@student.monash.edu>
2021-04-27 14:31:54 +1000

    category treemap props and style

--- client/src/components/CategoryTreeMapChart.tsx
+++ client/src/components/CategoryTreeMapChart.tsx
@@ -1,99 +1,4 @@
-import React from "react";
 import Chart from "react-apexcharts";
-// import { Treemap } from "react-vis";
-// import "react-vis/dist/style.css";
-
-// import { Treemap, ResponsiveContainer } from "recharts";
-
-// const data = [
-//   {
-//     name: "axis",
-//     children: [
-//       { name: "Axes", size: 1302 },
-//       { name: "Axis", size: 24593 },
-//       { name: "AxisGridLine", size: 652 },
-//       { name: "AxisLabel", size: 636 },
-//       { name: "CartesianAxes", size: 6703 },
-//     ],
-//   },
-//   {
-//     name: "controls",
-//     children: [
-//       { name: "AnchorControl", size: 2138 },
-//       { name: "ClickControl", size: 3824 },
-//       { name: "Control", size: 1353 },
-//       { name: "ControlList", size: 4665 },
-//       { name: "DragControl", size: 2649 },
-//       { name: "ExpandControl", size: 2832 },
-//       { name: "HoverControl", size: 4896 },
-//       { name: "IControl", size: 763 },
-//       { name: "PanZoomControl", size: 5222 },
-//       { name: "SelectionControl", size: 7862 },
-//       { name: "TooltipControl", size: 8435 },
-//     ],
-//   },
-// ];
-// const TreeMapChart = (props: any) => {
-//   return (
-//     <>
-//       <div style={{ width: 600, height: 400 }}>
-//         <ResponsiveContainer width="100%" height="100%">
-//           <Treemap
-//             width={600}
-//             height={400}
-//             data={data}
-//             dataKey="size"
-//             // ratio={4 / 3}
-//             stroke="#fff"
-//             fill="#8884d8"
-//           />
-//         </ResponsiveContainer>
-//       </div>
-//     </>
-//   );
-// };
-
-// const myData = {
-//   title: "analytics",
-//   color: "#12939A",
-//   children: [
-//     {
-//       title: "cluster",
-//       children: [
-//         { title: "AgglomerativeCluster", color: "#12939A", size: 3938 },
-//         { title: "CommunityStructure", color: "#12939A", size: 3812 },
-//         { title: "HierarchicalCluster", color: "#12939A", size: 6714 },
-//         { title: "MergeEdge", color: "#12939A", size: 743 },
-//       ],
-//     },
-//     {
-//       title: "graph",
-//       children: [
-//         { title: "BetweennessCentrality", color: "#12939A", size: 3534 },
-//         { title: "LinkDistance", color: "#12939A", size: 5731 },
-//         { title: "MaxFlowMinCut", color: "#12939A", size: 7840 },
-//         { title: "ShortestPaths", color: "#12939A", size: 5914 },
-//         { title: "SpanningTree", color: "#12939A", size: 3416 },
-//       ],
-//     },
-//     {
-//       title: "optimization",
-//       children: [{ title: "AspectRatioBanker", color: "#12939A", size: 7074 }],
-//     },
-//   ],
-// };
-// const TreeMapChart = (props: any) => {
-//   return (
-//     <>
-//       <Treemap
-//         title={"My New Treemap"}
-//         width={500}
-//         height={500}
-//         data={myData}
-//       />
-//     </>
-//   );
-// };
 const series = [
   {
     data: [
@@ -160,16 +65,45 @@ const options: ApexCharts.ApexOptions = {
   chart: {
     height: 350,
     type: "treemap",
+    events: {
+      dataPointSelection: (event, chartContext, config) => {
+        // console.log(config);
+        const { dataPointIndex } = config;
+        console.log(dataPointIndex);
+        // TODO: anything happens when clicking item in chart?
+      },
+    },
   },
   title: {
-    text: "Basic Treemap",
+    text: "Categories",
   },
 };
 
-const TreeMapChart = () => {
+interface CategoryDataItem {
+  id: number | string;
+  name: string;
+  count: number;
+}
+type CategoryTreeMapChartProps = {
+  data: Array<CategoryDataItem>;
+  height?: number;
+};
+const CategoryTreeMapChart = (props: CategoryTreeMapChartProps) => {
+  const { data, height } = props;
+  const chartData = data.map((dataPoint) => ({
+    id: dataPoint.id,
+    x: dataPoint.name,
+    y: dataPoint.count,
+  }));
+  const chartSeries = [{ data: chartData }];
   return (
-    <Chart options={options} series={series} type="treemap" height={350} />
+    <Chart
+      options={options}
+      series={chartSeries}
+      type="treemap"
+      height={height ? height : 400}
+    />
   );
 };
 
-export default TreeMapChart;
+export default CategoryTreeMapChart;
--- client/src/pages/Statistics.tsx
+++ client/src/pages/Statistics.tsx
@@ -1,11 +1,56 @@
+import { Grid, makeStyles, Paper } from "@material-ui/core";
 import React from "react";
-import TreeMapChart from "../components/TreeMapChart";
+import CategoryTreeMapChart from "../components/CategoryTreeMapChart";
+// TODO: replace with data from API call
+const mockCategoryData = [
+  {
+    id: 1,
+    name: "Technology",
+    count: 349,
+  },
+  {
+    id: 2,
+    name: "Food",
+    count: 1893,
+  },
+  {
+    id: 3,
+    name: "Entertainments",
+    count: 600,
+  },
+  {
+    id: "null",
+    name: "Uncategorised",
+    count: 6000,
+  },
+];
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
 
 const Statistics = () => {
+  const classes = useStyles();
+
   return (
     <div id="main">
       <h1>Statistics</h1>
-      <TreeMapChart />
+      <Grid container spacing={3}>
+        <Grid item xs={3}></Grid>
+        <Grid item xs={3}></Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+            <CategoryTreeMapChart data={mockCategoryData} />
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 };
