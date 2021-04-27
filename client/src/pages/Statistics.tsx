b0dc5d1b9fbf613935dd67804778df565b50b5c4
Uyen Tran <utra0001@student.monash.edu>
2021-04-27 15:18:04 +1000

    add bot alignment pie charts

--- client/src/pages/Statistics.tsx
+++ client/src/pages/Statistics.tsx
@@ -1,5 +1,6 @@
 import { Grid, makeStyles, Paper } from "@material-ui/core";
 import React from "react";
+import BotAlignmentPieChart from "../components/BotAlignmentPieChart";
 import CategoryTreeMapChart from "../components/CategoryTreeMapChart";
 // TODO: replace with data from API call
 const mockCategoryData = [
@@ -25,6 +26,44 @@ const mockCategoryData = [
   },
 ];
 
+const mockBotAlignmentData = [
+  {
+    label: "right",
+    count: 9,
+  },
+  {
+    label: "left",
+    count: 11,
+  },
+  {
+    label: "neutral",
+    count: 6,
+  },
+];
+
+const mockBotAlignmentData2 = [
+  {
+    label: "15-25",
+    count: 2,
+  },
+  {
+    label: "25-35",
+    count: 8,
+  },
+  {
+    label: "35-45",
+    count: 6,
+  },
+  {
+    label: "45-55",
+    count: 6,
+  },
+  {
+    label: "55+",
+    count: 2,
+  },
+];
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -43,8 +82,22 @@ const Statistics = () => {
     <div id="main">
       <h1>Statistics</h1>
       <Grid container spacing={3}>
-        <Grid item xs={3}></Grid>
-        <Grid item xs={3}></Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>
+            <BotAlignmentPieChart
+              data={mockBotAlignmentData}
+              title="Bot alignment by political beliefs"
+            />
+          </Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>
+            <BotAlignmentPieChart
+              data={mockBotAlignmentData2}
+              title="Bot alignment by age groups"
+            />
+          </Paper>
+        </Grid>
         <Grid item xs={6}>
           <Paper className={classes.paper}>
             <CategoryTreeMapChart data={mockCategoryData} />
