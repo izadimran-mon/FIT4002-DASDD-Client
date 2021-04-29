5e9a18184a8619254ea979e64aa6c82714ec4315
Anu Wickramasinghe <52063196+Anuwicks@users.noreply.github.com>
2021-04-30 00:14:00 +1000

    enhanced Ad card layout added buttons made image clickable

--- client/src/components/AdCard.tsx
+++ client/src/components/AdCard.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
-
+import Tooltip from "@material-ui/core/Tooltip";
 import moment from "moment";
 import "./styles/AdCard.css";
 import AdChip from "./AdChip";
-import { Grid, Card, Typography } from "@material-ui/core";
+import {
+  Grid,
+  Card,
+  Typography,
+  Button,
+  Dialog,
+  DialogContent,
+  CardActionArea,
+} from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -22,76 +30,189 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-// export default function AdCard(props: Ad) {
-//   const classes = useStyles();
+const processLink = (link: string) => {
+  if (link) {
+    let link_split = link.split("/");
+    let domain = link_split[2];
+    if (domain) {
+      let prefix = domain.split(".");
+      if (prefix[0].toLowerCase() === "www") {
+        prefix.shift();
+      }
+      domain = prefix.join(".");
+    }
 
-//   return (
-//     <div className={classes.root}>
-//       <Paper elevation={3}>{props.bot}</Paper>
-//     </div>
-//   );
-// }
+    return domain;
+  }
+};
+
+const ImageDialog = (props: any) => {
+  return (
+    <Dialog
+      onClose={props.handleClose}
+      aria-labelledby='simple-dialog-title'
+      open={props.open}
+    >
+      <DialogContent>
+        <img
+          style={{
+            width: "auto",
+            height: "100%",
+          }}
+          src={props.image}
+          alt='Ad screenshot full'
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 const AdCard = (props: any) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const classes = useStyles();
   return (
     <Card style={{ height: 350, marginBottom: 20, padding: 15 }}>
       <Grid container style={{ height: "100%" }}>
-        <Grid item xs={4}>
-          <img
-            style={{ maxWidth: 300 }}
-            src={props.image}
-            alt='Ad screenshot'
-            onClick={() => {
-              window.open(props.image, "_blank");
-            }}
-          />
+        <Grid
+          item
+          xs={4}
+          style={{
+            maxHeight: 350,
+            background: "#f7f7f7",
+          }}
+        >
+          {" "}
+          <CardActionArea style={{ height: "100%" }}>
+            <img
+              style={{
+                width: "auto",
+                height: "auto",
+                maxWidth: 300,
+                maxHeight: 320,
+              }}
+              src={props.image}
+              alt='Ad screenshot'
+              onClick={() => {
+                handleClickOpen();
+              }}
+            />{" "}
+          </CardActionArea>
         </Grid>
         <Grid item xs={8}>
-          <Grid container style={{ height: "100%" }}>
-            <Grid item xs={6}>
-              {props.headline ? (
-                <Typography
-                  style={{ textOverflow: "ellipsis", overflow: "hidden" }}
-                >
-                  <a
-                    href={`https://${props.headline} `}
-                    target='_blank'
-                    rel='noreferrer'
-                  >
-                    {props.headline}
-                  </a>
-                </Typography>
-              ) : (
-                <Typography style={{ fontSize: 18, fontWeight: 600 }}>
-                  {" "}
-                  No Link Available
-                </Typography>
-              )}
-            </Grid>
-            <Grid item xs={6}>
-              {" "}
-              <Typography>
+          <Grid
+            container
+            style={{ height: "100%", marginLeft: 15, width: "auto" }}
+          >
+            <Grid container direction='row' style={{ height: "42%" }}>
+              <Grid item xs={6}>
+                {props.headline ? (
+                  <div>
+                    <Tooltip
+                      title={
+                        <Typography>{`https://${props.headline}`}</Typography>
+                      }
+                    >
+                      <Button
+                        variant='contained'
+                        color='primary'
+                        href={`https://${props.headline} `}
+                        target='_blank'
+                        rel='noreferrer'
+                      >
+                        Goto Ad Link
+                      </Button>
+                    </Tooltip>
+                  </div>
+                ) : (
+                  <Typography style={{ fontSize: 18, fontWeight: 600 }}>
+                    {" "}
+                    No Link Available
+                  </Typography>
+                )}
+              </Grid>
+              <Grid item xs={6}>
                 {" "}
-                Date: {moment(props.createdAt).format(
-                  "YYYY-MMM-D dddd h:mma"
-                )}{" "}
-              </Typography>
-              <Typography>Seen bot: {props.botId}</Typography>
-              <Typography>
-                Seen on: <a href={props.seenOn}>{props.seenOn}</a>
-              </Typography>
-            </Grid>
-            <Grid
-              item
-              xs={12}
-              style={{ border: "1px solid #b2b2b2", borderRadius: 10 }}
-            >
-              <div>{<AdChip />}</div>
+                <Typography style={{ marginTop: 5 }}>
+                  {" "}
+                  <span style={{ fontWeight: "bold" }}>Date:</span>{" "}
+                  {moment(props.createdAt).format("YYYY-MMM-D dddd h:mma")}{" "}
+                </Typography>
+                <Typography style={{ marginTop: 5 }}>
+                  <span style={{ fontWeight: "bold" }}>Seen bot:</span>{" "}
+                  {props.botId}
+                </Typography>
+                {props.seenOn ? (
+                  <div>
+                    <Grid container style={{ marginTop: 5 }}>
+                      <Grid item>
+                        <Typography style={{ fontWeight: "bold" }}>
+                          Seen on:{" "}
+                        </Typography>
+                      </Grid>{" "}
+                      <Grid item>
+                        {" "}
+                        <Tooltip
+                          title={<Typography>{props.seenOn}</Typography>}
+                        >
+                          <Button
+                            variant='contained'
+                            style={{
+                              background: "#167070",
+                              marginLeft: 10,
+                              padding: 2,
+                              paddingLeft: 10,
+                              paddingRight: 10,
+                            }}
+                            href={props.seenOn}
+                            target='_blank'
+                            rel='noreferrer'
+                          >
+                            {" "}
+                            <Typography
+                              style={{
+                                color: "#fff",
+                                textTransform: "lowercase",
+                                fontSize: 14,
+                              }}
+                            >
+                              {" "}
+                              {processLink(props.seenOn)}
+                            </Typography>
+                          </Button>
+                        </Tooltip>
+                      </Grid>
+                    </Grid>{" "}
+                  </div>
+                ) : (
+                  <Typography> No Link</Typography>
+                )}
+              </Grid>
+            </Grid>{" "}
+            <Grid container direction='row' style={{ height: "58%" }}>
+              <Grid
+                item
+                xs={12}
+                style={{
+                  border: "1px solid #b2b2b2",
+                  borderRadius: 10,
+                  padding: 10,
+                }}
+              >
+                <div>{<AdChip />}</div>
+              </Grid>{" "}
             </Grid>
           </Grid>
         </Grid>
       </Grid>
+      <ImageDialog image={props.image} open={open} handleClose={handleClose} />
     </Card>
   );
 };
--- client/src/pages/Ads.tsx
+++ client/src/pages/Ads.tsx
@@ -3,13 +3,16 @@ import AdCard from "../components/AdCard";
 import Pagination from "@material-ui/lab/Pagination";
 import { mockData } from "../mockData";
 import axios from "axios";
+import { Grid } from "@material-ui/core";
 
 //ads?offset=0&limit=30
 const Ads = () => {
-  const [offset, setOffset] = useState(0);
+  let storedPageNumber = localStorage.getItem("adsPage");
   const [limit, setLimit] = useState(30);
   const [ads, setAds] = useState([]);
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(
+    storedPageNumber ? JSON.parse(storedPageNumber) : 1
+  );
 
   // useEffect(() => {
   //   axios.get(`/ads?offset${(page-1)*limit}&limit${limit}`).then((res: any) => {
@@ -23,24 +26,35 @@ const Ads = () => {
       .then((res: any) => {
         setAds(res.data);
       });
-  }, [page]);
+  }, [page, limit]);
 
   const handleChange = (event: any, value: number) => {
+    localStorage.setItem("adsPage", JSON.stringify(value));
     setPage(value);
   };
-
-  const tryData = ads.slice(0, 10);
-  console.log(tryData);
-
+  console.log(ads);
   return (
     <div id='main'>
       <h1>Ads</h1>
-      <Pagination count={4334} page={page} onChange={handleChange} />
-      {tryData.map((data, i) => {
+      <Grid container justify='flex-end' style={{ marginBottom: 15 }}>
+        <Pagination
+          count={4334}
+          page={page}
+          onChange={handleChange}
+          size='large'
+        />
+      </Grid>
+      {ads.map((data, i) => {
         return <AdCard {...data} key={i} />;
       })}
-      <Pagination count={4334} page={page} onChange={handleChange} />
-      {/* <DataGrid rows={mockData} pageSize={5}/> */}
+      <Grid container justify='flex-end'>
+        <Pagination
+          count={4334}
+          page={page}
+          onChange={handleChange}
+          size='large'
+        />
+      </Grid>
     </div>
   );
 };
